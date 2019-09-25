8fe3445b2364aa919188939bfa3787910a6a1e6b
Huang Zihao <huangzihaohzh@163.com>
2019-09-25 23:07:10 +0800

    更新测试类

--- src/wordcounter/test/Test.java
+++ src/wordcounter/test/Test.java
@@ -2,6 +2,8 @@ package wordcounter.test;
 
 import wordcounter.entity.TextFile;
 
+import java.io.IOException;
+
 public class Test {
     private TextFile textFile = null;
 
@@ -19,6 +21,21 @@ public class Test {
             System.out.println("字符数计算出错");
     }
 
-    //测试TextFile.charNumCounter()
-    
+    //测试TextFile.wordNumCounter()
+    public void testWordNumCounter(){
+        int wordNum = textFile.wordNumCounter();
+        if(wordNum>=0)
+            System.out.println("文件单词数为："+wordNum);
+        else
+            System.out.println("单词计算出错");
+    }
+
+    //测试TextFile.lineNumCounter()
+    public void testLineNumCounter() throws IOException {
+        int lineNum = textFile.lineNumCounter();
+        if(lineNum>=0)
+            System.out.println("文件行数为："+lineNum);
+        else
+            System.out.println("行数计算错误");
+    }
 }
--- src/wordcounter/test/TestRunner.java
+++ src/wordcounter/test/TestRunner.java
@@ -1,4 +1,17 @@
-package wordcounter.entity;
+package wordcounter.test;
+
+import wordcounter.test.Test;
+
+import java.io.IOException;
 
 public class TestRunner {
+    private static String path = "D:\\123.txt";
+
+    public static void main(String[] args) throws Exception {
+        Test test = new Test(path);
+        System.out.println(path);
+        test.testCharNumCounter();
+        test.testWordNumCounter();
+        test.testLineNumCounter();
+    }
 }
